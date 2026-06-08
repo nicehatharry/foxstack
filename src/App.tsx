4d352472f7d37362dafa3a45cd3259d02ccea423
Brian Fox <bfox.fullstack@protonmail.com>
2026-06-07 21:24:38 -0500

    page routing with wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import { Homepage } from './homepage'
 import './baseStyles.css'
+import { Link, Route, Switch } from "wouter"
+import GroceryList from './groceryList/GroceryList'
 
-function App() {
-	return <Homepage />
-}
+const App = () => (
+  <>
+    {/* 
+      Routes below are matched exclusively -
+      the first matched route gets rendered
+    */}
+    <Switch>
+      <Route path="/grocery-list" component={GroceryList} />
+
+      <Route path="/" component={Homepage} />
+
+      {/* Default route in a switch */}
+      <Route>404: No such page!</Route>
+    </Switch>
+  </>
+)
 
 export default App
